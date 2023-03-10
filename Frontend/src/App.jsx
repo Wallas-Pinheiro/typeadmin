696b0ebf855b9eead214f3d6332d90e2b20f2e0e
Wallas-Pinheiro <wallllaw@hotmail.com>
2023-03-09 21:22:23 -0300

    Logout Button

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,42 @@
 import { Login } from './pages/login'
-import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
-import { ListUsers } from './pages/ListUsers';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { ListUsers } from './pages/ListUsers'
+import Cookies from 'js-cookie'
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <Login />,
-},
-{
-  path:"/app",
-  element: <ListUsers/>
+function PrivateRoute({ children }) {
+	const token = Cookies.get('token')
+	if (token) {
+		return children
+	}
+	return <Navigate to="/" />
 }
-]);
+
+function ProtectedRoute({ children }) {
+	const token = Cookies.get('token')
+	if (token) {
+		return <Navigate to="/app" />
+	}
+	return children
+}
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: (
+			<ProtectedRoute>
+				<Login />
+			</ProtectedRoute>
+		)
+	},
+	{
+		path: '/app',
+		element: (
+			<PrivateRoute>
+				<ListUsers />
+			</PrivateRoute>
+		)
+	}
+])
 
 export function App() {
 	return <RouterProvider router={router} />
